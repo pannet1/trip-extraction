f08418942c76bfb7fa7ef0a64e48fcd8aa70c17d
Maheswaran Palaniselvan <maheswaran.palaniselvan@gmail.com>
2024-04-03 00:44:13 +0530

    changes

--- trip_extraction/main.py
+++ trip_extraction/main.py
@@ -1,70 +1,56 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+#! pip install pandas undetected_chromedriver bs4 selenium
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import time
 
-"""
-knowledge source:
-https://medium.com/@writerbricks/scraping-dynamic-data-that-loads-on-scroll-with-python-c4c4970a54d1
-"""
+wait_time = 1000
+timeout = 3000
+total_iterations = 20
 # actual url to be scaped
-url = "https://www.trip.com/hotels/list?city=220&cityName=Dubai&provinceId=10965&countryId=9&districtId=0&checkin=2024%2F05%2F23&checkout=2024%2F05%2F24&barCurr=USD&searchType=CT&searchWord=Dubai&searchValue=19%7C220_19_220_1&searchCoordinate=3_-1_-1_0%7C2_-1_-1_0%7C1_-1_-1_0%7CNORMAL_25.2048493_55.2707828_0&crn=1&adult=2&children=0&searchBoxArg=t&travelPurpose=0&ctm_ref=ix_sb_dl&domestic=true&listFilters=17%7C1*17*1*2%2C80%7C0%7C1*80*0*2%2C29%7C1*29*1%7C2*2&locale=en-XX&curr=USD"
+url = "https://www.trip.com/hotels/list?city=220&cityName=Dubai&provinceId=0&countryId=0&districtId=0&checkin=2024%2F04%2F03&checkout=2024%2F04%2F11&crn=1&adult=2&children=0&searchBoxArg=t&travelPurpose=0&ctm_ref=ix_sb_dl&domestic=true&listFilters=17%7C1*17*1*2%2C80%7C0%7C1*80*0*2%2C29%7C1*29*1%7C2*2&locale=en-XX&curr=USD"
 
-
+hotel_details = {}
+city_id = re.split("=|&", url)[1]
 def scrape_quotes(url):
-    # Set up the Selenium WebDriver
-    driver = (
-        webdriver.Chrome()
-    )  # Make sure you have chromedriver installed and in your PATH
+    driver = uc.Chrome()
     driver.get(url)
-
-    # Wait for the initial content to load
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "quote"))
-    )
-
-    # Extract and print initial data
+    WebDriverWait(driver, timeout, wait_time)
     initial_html = driver.page_source
     initial_soup = BeautifulSoup(initial_html, "html.parser")
-    initial_quotes = initial_soup.find_all("div", class_="quote")
+    initial_quotes = initial_soup.find_all("div", class_="compressmeta-hotel-wrap-v8")
     extract_and_print_quotes(initial_quotes)
-
-    # Simulate scroll events to load additional content
-    for scroll_count in range(4):  # Assuming there are 5 scroll events in total
-        # Scroll down using JavaScript
+    for _ in range(total_iterations):
         driver.execute_script(
             "window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait for the dynamically loaded content to appear
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "quote"))
-        )
-
-        # Extract and print the newly loaded quotes
+        driver.implicitly_wait(10)
+        time.sleep(10)
+        WebDriverWait(driver, timeout, wait_time)
         scroll_html = driver.page_source
         scroll_soup = BeautifulSoup(scroll_html, "html.parser")
-        scroll_quotes = scroll_soup.find_all("div", class_="quote")
+        scroll_quotes = scroll_soup.find_all("div", class_="compressmeta-hotel-wrap-v8")
         extract_and_print_quotes(scroll_quotes)
-
-    # Close the WebDriver
     driver.quit()
 
 
 def extract_and_print_quotes(quotes):
-
     for quote in quotes:
-        text = quote.find("span", class_="text").get_text(strip=True)
-        author = quote.find("small", class_="author").get_text(strip=True)
-        tags = [tag.get_text(strip=True)
-                for tag in quote.find_all("a", class_="tag")]
-
-        print(f"Quote: {text}")
-        print(f"Author: {author}")
-        print(f"Tags: {', '.join(tags)}")
+        quote = BeautifulSoup(str(quote), "html.parser")
+        hotel_id = quote.find("div", class_="compressmeta-hotel-wrap-v8")["id"]
+        hotel_name = quote.find("span", class_="name").text
+        hotel_details[hotel_name] = (
+            f"https://www.trip.com/hotels/detail/?cityId={city_id}&hotelId={hotel_id}"
+        )
+        print(f"Hotel ID: {hotel_id}")
+        print(f"Hotel Name: {hotel_name}")
+        print(f"{len(hotel_details)=}")
         print("----------")
 
 
 if __name__ == "__main__":
-    target_url = "http://quotes.toscrape.com/scroll"
-    scrape_quotes(target_url)
+    scrape_quotes(url)
+    print(hotel_details)
+    import pandas as pd
+    pd.DataFrame.from_dict(hotel_details, orient="index").to_csv("123.csv")
